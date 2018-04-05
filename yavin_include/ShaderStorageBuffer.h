ea5fd784a8e3e39a30e712347b1232a759e7d466
Steve Wolligandt <stevewolligandt@gmail.com>
2018-04-05 08:57:57 +0200

    ssbo working with usage

--- yavin_include/ShaderStorageBuffer.h
+++ yavin_include/ShaderStorageBuffer.h
@@ -16,11 +16,13 @@ namespace Yavin {
 template <typename T>
 class ShaderStorageBuffer : public Buffer<GL_SHADER_STORAGE_BUFFER, T> {
  public:
-  using parent_t = Buffer<GL_SHADER_STORAGE_BUFFER, T>;
+  using parent_t                     = Buffer<GL_SHADER_STORAGE_BUFFER, T>;
+  using usage_t                      = typename parent_t::usage_t;
+  static const usage_t default_usage = usage_t::DYNAMIC_DRAW;
 
   //----------------------------------------------------------------------------
 
-  ShaderStorageBuffer() : parent_t() {}
+  ShaderStorageBuffer(usage_t usage = default_usage) : parent_t(usage) {}
 
   //----------------------------------------------------------------------------
 
@@ -33,19 +35,20 @@ class ShaderStorageBuffer : public Buffer<GL_SHADER_STORAGE_BUFFER, T> {
 
   //----------------------------------------------------------------------------
 
-  ShaderStorageBuffer(size_t n) : parent_t(n) {}
+  ShaderStorageBuffer(size_t n, usage_t usage = default_usage)
+      : parent_t(n, usage) {}
 
   //----------------------------------------------------------------------------
 
-  ShaderStorageBuffer(const std::vector<T>& data, bool direct_upload = true,
-                      bool keep_data_on_cpu = false)
-      : parent_t(data, direct_upload, keep_data_on_cpu) {}
+  ShaderStorageBuffer(const std::vector<T>& data, usage_t usage = default_usage,
+                      bool direct_upload = true, bool keep_data_on_cpu = false)
+      : parent_t(data, usage, direct_upload, keep_data_on_cpu) {}
 
   //----------------------------------------------------------------------------
 
-  ShaderStorageBuffer(std::vector<T>&& data, bool direct_upload = true,
-                      bool keep_data_on_cpu = false)
-      : parent_t(std::move(data), direct_upload, keep_data_on_cpu) {}
+  ShaderStorageBuffer(std::vector<T>&& data, usage_t usage = default_usage,
+                      bool direct_upload = true, bool keep_data_on_cpu = false)
+      : parent_t(std::move(data), usage, direct_upload, keep_data_on_cpu) {}
 
   //----------------------------------------------------------------------------
 
