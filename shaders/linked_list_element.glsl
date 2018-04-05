fbf17ffad164077e7e6730f64da36074f8c340f1
Steve Wolligandt <stevewolligandt@gmail.com>
2018-04-05 19:49:10 +0200

    linked_list_write correctly checks equal depths

--- shaders/linked_list_element.glsl
+++ shaders/linked_list_element.glsl
@@ -1,5 +1,6 @@
-#ifndef LINKED_LIST_ELEMENT_GLSL__
-#define LINKED_LIST_ELEMENT_GLSL__
+#ifndef LINKED_LIST_ELEMENT_GLSL
+#define LINKED_LIST_ELEMENT_GLSL
+
 struct linked_list_element {
   vec4  color;
   uint  next_index;
--- shaders/linked_list_write.glsl
+++ shaders/linked_list_write.glsl
@@ -5,20 +5,24 @@ layout (binding = 5, offset = 0) uniform atomic_uint cnt;
 
 vec4 shading();
 void main() {
-  uint i = atomicCounterIncrement(cnt);
-  if (i < size) {
-    uint running_index = imageLoad(head_index, ivec2(gl_FragCoord.xy)).r;
-    bool duplicate = false;
-    // while (running_index != 0xffffffff) {
-    //   if (abs(nodes[running_index].depth - gl_FragCoord.z) < 1e-7) {
-    //     duplicate = true;
-    //     break;
-    //   }
-    //   running_index = nodes[running_index].next_index;
-    // }
+  uint running_index = imageLoad(head_index, ivec2(gl_FragCoord.xy)).r;
+  bool duplicate = false;
+  for (uint i = 0; i < 20; ++i) {
+  // while (running_index != 0xffffffff) {
+    if (running_index == 0xffffffff) break;
+    if (abs(nodes[running_index].depth - gl_FragCoord.z) < 1e-7) {
+    //if (nodes[running_index].depth == 1e-3) {
+      duplicate = true;
+      break;
+    }
+    running_index = nodes[running_index].next_index;
+  }
 
-    if (!duplicate) {
-      uint next_index = imageAtomicExchange(head_index, ivec2(gl_FragCoord.xy), i);
+  if (!duplicate) {
+    uint i = atomicCounterIncrement(cnt);
+    if (i < size) {
+      uint next_index 
+        = imageAtomicExchange(head_index, ivec2(gl_FragCoord.xy), i);
       nodes[i].color = shading();
       nodes[i].depth = gl_FragCoord.z;
       nodes[i].next_index = next_index;
