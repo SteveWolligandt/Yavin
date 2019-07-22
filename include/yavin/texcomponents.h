339c15f957aeded129a227dee4e945ecaf2c3c7f
Steve Wolligandt <steve@isg.cs.uni-magdeburg.de>
2019-07-22 16:41:06 +0200

    -

--- include/yavin/texcomponents.h
+++ include/yavin/texcomponents.h
@@ -1,30 +1,32 @@
 #ifndef __YAVIN_TEX_COMPONENTS_H__
 #define __YAVIN_TEX_COMPONENTS_H__
 
+#include <cstdint>
+
 //==============================================================================
 namespace yavin {
 //==============================================================================
 
 struct R {
-  static constexpr size_t num_components = 1;
+  static constexpr std::size_t num_components = 1;
 };
 struct RG {
-  static constexpr size_t num_components = 2;
+  static constexpr std::size_t num_components = 2;
 };
 struct RGB {
-  static constexpr size_t num_components = 3;
+  static constexpr std::size_t num_components = 3;
 };
 struct RGBA {
-  static constexpr size_t num_components = 4;
+  static constexpr std::size_t num_components = 4;
 };
 struct BGR {
-  static constexpr size_t num_components = 3;
+  static constexpr std::size_t num_components = 3;
 };
 struct BGRA {
-  static constexpr size_t num_components = 4;
+  static constexpr std::size_t num_components = 4;
 };
 struct Depth {
-  static constexpr size_t num_components = 1;
+  static constexpr std::size_t num_components = 1;
 };
 
 //==============================================================================
