284ad8102a3e32f0f7fb41f2e1a10b309149a943
Steve Wolligandt <steve@isg.cs.uni-magdeburg.de>
2019-12-08 00:28:00 +0100

    framebuffer example working with egl

--- test/framebuffer.cpp
+++ test/framebuffer.cpp
@@ -4,7 +4,7 @@
 namespace yavin::test {
 //==============================================================================
 TEST_CASE("framebuffer0", "[framebuffer]") {
-  vertexbuffer<vec2> vbo{{-1,-1}, {1,-1}, {1,1}, {-1,1}};
+  vertexbuffer<vec2> vbo{{0,0}, {1,0}, {1,1}, {0,1}};
   indexbuffer ibo{0,1,2, /*2,3,0*/};
   vertexarray vao;
   vao.bind();
@@ -12,32 +12,38 @@ TEST_CASE("framebuffer0", "[framebuffer]") {
   ibo.bind();
   vbo.activate_attributes();
 
-  tex2rgb<float> col; 
-  texdepth depth;
-  framebuffer fbo{col, depth};
-  fbo.bind();
-  gl::clear_color(0, 0, 0, 0);
-  clear_color_depth_buffer();
-
+  orthographiccamera cam{0,1,0,1,-1,1, 1024,1024};
   shader s;
   s.add_stage<vertexshader>(
       "#version 450\n"
+      "uniform mat4 proj;\n"
       "layout(location = 0) in vec2 pos;\n"
       "void main() {\n"
-      "  gl_Position = projection * vec4(pos, 1);\n"
+      "  gl_Position = proj * vec4(pos, 0, 1);\n"
       "}",
       shaderstageparser::SOURCE);
+
   s.add_stage<fragmentshader>(
-      "#version 450"
-      "layout(location = 0) out vec4 frag_color;\n"
-      "void main() {"
-      "  frag_color = vec3(1,1,1);"
+      "#version 450\n"
+      "layout(location = 0) out vec3 frag_color;\n"
+      "void main() {\n"
+      "  frag_color = vec3(0,0,1);\n"
       "}",
       shaderstageparser::SOURCE);
   s.create();
   s.bind();
+  s.set_uniform("proj", cam.projection_matrix());
 
-  vao.draw_triangles(2);
+  tex2rgb<float> col{1024, 1024}; 
+  texdepth depth{1024, 1024};
+  framebuffer fbo{col, depth};
+  fbo.bind();
+  gl::clear_color(255, 0, 0, 0);
+  clear_color_buffer();
+
+  gl::viewport(cam.viewport());
+  vao.draw_triangles(3);
+  fbo.unbind();
   col.write_png("fbo_test.png");
 }
 //==============================================================================
