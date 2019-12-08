36371e4f3a1bb98b2427e58926dd832a5c577fe7
Steve Wolligandt <stevewolligandt@gmail.com>
2019-12-08 23:26:27 +0100

    -

--- src/errorcheck.cpp
+++ src/errorcheck.cpp
@@ -26,15 +26,16 @@ gl_framebuffer_not_complete_error::gl_framebuffer_not_complete_error(
 
 const std::string gl_error_to_string(GLenum err) {
   switch (err) {
-    case GL_INVALID_ENUM: return "invalid enum";
-    case GL_INVALID_VALUE: return "invalid value";
+    case GL_INVALID_ENUM:      return "invalid enum";
+    case GL_INVALID_VALUE:     return "invalid value";
     case GL_INVALID_OPERATION: return "invalid operation";
-    case GL_STACK_OVERFLOW: return "stack overflow";
-    case GL_STACK_UNDERFLOW: return "stack underflow";
-    case GL_OUT_OF_MEMORY:
-      return "out of memory";
-      // case GL_TABLE_TOO_LARGE:
-      //   return "table too large";
+    case GL_STACK_OVERFLOW:    return "stack overflow";
+    case GL_STACK_UNDERFLOW:   return "stack underflow";
+    case GL_OUT_OF_MEMORY:     return "out of memory";
+    case GL_CONTEXT_LOST:      return "context lost";
+    case GL_TABLE_TOO_LARGE:   return "tablet too large";
+    case GL_INVALID_FRAMEBUFFER_OPERATION:
+      return "invalid framebuffer operation";
     default: return "unknown GL error";
   }
 }
