533b171fa929ad5f5a945d560dcc311609e7ea5a
Steve Wolligandt <steve@isg.cs.ovgu.de>
2020-12-10 16:49:34 +0100

    -

--- include/yavin/imgui_includes.h
+++ include/yavin/imgui_includes.h
@@ -2,9 +2,9 @@
 #define YAVIN_IMGUI_INCLUDES_H
 //==============================================================================
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include <imgui/imgui.h>
-#include <imgui/imgui_internal.h>
-#include <imgui/misc/cpp/imgui_stdlib.h>
+#include <imgui.h>
+#include <imgui_internal.h>
+#include <misc/cpp/imgui_stdlib.h>
 #include <imfilebrowser.h>
 //==============================================================================
 #endif
